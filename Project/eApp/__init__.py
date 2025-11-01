6bd8b28e56892623ebf4d87c2ec0fbe6b474a237
Nhan Nguyen <nguyenvuthiennhan05@gmail.com>
2025-11-01 14:05:24 +0700

    Khởi tạo

---
+++ Project/eApp/__init__.py
@@ -0,0 +1,13 @@
+# Khởi tạo web application
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', msg = 'Welcome to the eApp!')    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)     
